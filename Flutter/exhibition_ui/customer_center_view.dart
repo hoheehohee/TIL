54581ed4b2640a76421596e75bcdc53c27745730
hoheehohee <www.hohee@gmail.com>
2020-12-15 08:16:54 +0000

    - flutter: customer_center_view

--- Flutter/exhibition_ui/customer_center_view.dart
+++ Flutter/exhibition_ui/customer_center_view.dart
@@ -52,7 +52,41 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MyWidget extends StatelessWidget {
+
+class Item {
+  Item({
+    this.expandedValue,
+    this.headerValue,
+    this.isExpanded = false,
+  });
+
+  String expandedValue;
+  String headerValue;
+  bool isExpanded;
+}
+
+List<Item> generateItems(int numberOfItems) {
+  return List.generate(numberOfItems, (int index) {
+    return Item(
+      headerValue: 'Panel $index',
+      expandedValue: 'This is item number $index',
+    );
+  });
+}
+
+class MyWidget extends StatefulWidget {
+  MyWidget({Key key}) : super(key: key);
+
+  @override
+  MyWidgetState createState() => MyWidgetState();
+}
+
+class MyWidgetState extends State<MyWidget> {
+  
+  
+  
+  List<Item> _data = generateItems(8);
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -118,7 +152,36 @@ class MyWidget extends StatelessWidget {
                     ]
                   )
                 ),
-                
+                Container(
+                  width: double.infinity,
+                  margin: EdgeInsets.all(10),
+                  child: ExpansionPanelList(
+                    expansionCallback: (int index, bool isExpanded) {
+                      setState(() {
+                        _data[index].isExpanded = !isExpanded;
+                      });
+                    },
+                    children: _data.map<ExpansionPanel>((Item item) {
+                      return ExpansionPanel(
+                        headerBuilder: (BuildContext context, bool isExpanded) {
+                          return ListTile(
+                            title: Text(item.headerValue),
+                          );
+                        },
+                        body: ListTile(
+                          title: Text(item.expandedValue),
+                          subtitle: Text('To delete this panel, tap the trash can icon'),
+                          trailing: Icon(Icons.delete),
+                          onTap: () {
+                            setState(() {
+                              _data.removeWhere((currentItem) => item == currentItem);
+                            });
+                          }),
+                        isExpanded: item.isExpanded,
+                      );
+                    }).toList(),
+                  )
+                )
               ]
             )
           )
